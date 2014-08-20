89058297d25cfb6e08d4e0659434ff940d5f7487
Leonid Shevtsov <leonid@shevtsov.me>
2014-08-20 13:33:15 +0300

    fix sauce and server configuration

--- spec/sauce_helper.rb
+++ spec/sauce_helper.rb
@@ -3,19 +3,23 @@ require "sauce/capybara"
 
 SAUCE_CONNECT_PORT = 2001
 
-Sauce.config do |c|
-  c[:browsers] = [
-    ["Windows 8", "Internet Explorer", "10"],
-    ["Windows 7", "Firefox", "20"],
-    ["OS X 10.8", "Safari", "6"],
-    ["Linux", "Chrome", nil]
-  ]
-  c[:application_port] = SAUCE_CONNECT_PORT
-end
-
-# No one will start the app for us with the Sauce driver
-Thread.new do
-  Capybara.run_default_server(Capybara.app, SAUCE_CONNECT_PORT)
+Sauce.config do |config|
+  config[:os], config[:browser], config[:browser_version] = ENV['SAUCE_BROWSER'].split(':')
+  config[:application_port] = SAUCE_CONNECT_PORT
 end
 
 Capybara.javascript_driver = :sauce
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    thread = Thread.new do
+      Capybara.run_default_server(Capybara.app, SAUCE_CONNECT_PORT)
+    end
+    RSpec.instance_variable_set '@server_thread', thread
+  end
+
+  config.after(:suite) do
+    thread = RSpec.instance_variable_get('@server_thread')
+    Thread.kill(thread) if thread
+  end
+end
