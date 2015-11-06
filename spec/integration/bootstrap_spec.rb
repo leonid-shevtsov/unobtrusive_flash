03db67a61d47bbf9bd0759cae6e100c34b010d1f
Leonid Shevtsov <leonid@shevtsov.me>
2015-11-06 08:31:52 +0200

    fix capybara spec not waiting for flash message to disappear

--- spec/integration/bootstrap_spec.rb
+++ spec/integration/bootstrap_spec.rb
@@ -18,7 +18,6 @@ describe "bootstrap spec", type: :feature, js: true do
     visit '/test/bootstrap'
     expect(page).to have_content 'Inline Notice'
     find('.alert.alert-info .close').click
-    sleep 1 # for the flash message to fade out
-    expect(page).to_not have_content 'Inline Notice'
+    expect(page).to have_no_content 'Inline Notice'
   end
 end
