ae47daecf9405d0e4af1c218bec40daf9c4168fd
Cort Spellman <spellman.cort@gmail.com>
2016-03-13 19:41:04 -0500

    Remove messages instead of emptying the container.
    There may be additional content in the flash-messages container,
    especially if the user is using Bootstrap and does not define a
    .unobtrusive-flash-container element. Therefore, instead of emptying the
    messages container, delete the message elements by matching their CSS
    class.
    NOTE: With Bootstrap, all .alert elements in the messages container will
    be cleared before a new message is shown when
    UnobtrusiveFlash.flashOptions.clearFlashOnNextRequest is truthy.

--- spec/integration/bootstrap_spec.rb
+++ spec/integration/bootstrap_spec.rb
@@ -35,4 +35,18 @@ describe "bootstrap spec", type: :feature, js: true do
     page.execute_script("$.get('/test/ajax_flash');")
     expect(page).to have_content 'Ajax Error', count: 1
   end
+
+  specify 'clearing existing messages should only clear messages (and not other content) from the flash container' do
+    visit '/test/bootstrap'
+
+    non_message_id = "not-a-message"
+    non_message_content = "not a message"
+    non_message_element = "<p id=\"#{non_message_id}\">#{non_message_content}</p>"
+
+    page.execute_script('UnobtrusiveFlash.flashOptions.clearFlashOnNextRequest = true;')
+    page.execute_script("$('.alert').parent().prepend('#{non_message_element}');")
+    expect(find("##{non_message_id}")).to have_content non_message_content, count: 1
+    page.execute_script("$.get('/test/ajax_flash');")
+    expect(find("##{non_message_id}")).to have_content non_message_content, count: 1
+  end
 end
