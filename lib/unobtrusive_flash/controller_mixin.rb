3c29f798e2b1dd37316b4a790c54181f6b6feb0d
Raf Szalanski <rafal.szalanski@gmail.com>
2016-06-02 12:50:43 +0100

    Use request.host for apps hosted behind AWS ELB

--- lib/unobtrusive_flash/controller_mixin.rb
+++ lib/unobtrusive_flash/controller_mixin.rb
@@ -20,9 +20,15 @@ module UnobtrusiveFlash
 
     # Setting cookies for :all domains is broken for Heroku apps, read this article for details
     # https://devcenter.heroku.com/articles/cookies-and-herokuapp-com
+    #
+    # Setting cookies for :all domains also appears to be broken for apps hosted on EC2 behind
+    # AWS ELB. No written documentation on that yet, only quantitive analysis based on
+    # obvservation of a few instances of load balancers and EC2 instances accessed directly
+    # via their IP addresses.
+    #
     # You can also override this method in your controller if you need to customize the cookie domain
     def unobtrusive_flash_domain
-      if request.host =~ /\.herokuapp\.com$/
+      if request.host =~ /\.herokuapp\.com$/ || request.host =~ /\.amazonaws\.com$/
         request.host
       else
         :all
