bdb225556bbd9da15639ba9e77db0c4d33bd7396
Jared Hales <jared@bespokepost.com>
2018-01-18 16:19:36 -0500

    Better handle `cacheable-flash` transitioners
    The `cacheable-flash` gem is no longer maintained and the author suggests `unobtrusive_flash` as its successor: https://github.com/pboling/cacheable-flash
    
    However, the flash cookie format is different between these two libraries. This patch will make the transition from one to the other more painless (and as an added side effect also to help to handle tampered cookies that may not be in the expected format). I manually did this locally for our project and figured others might benefit from it as well.

--- lib/unobtrusive_flash/controller_mixin.rb
+++ lib/unobtrusive_flash/controller_mixin.rb
@@ -50,15 +50,16 @@ module UnobtrusiveFlash
       end
 
       def append_flash_to_cookie(existing_cookie, flash, unobtrusive_flash_keys)
-        cookie_flash = (existing_cookie && parse_cookie(existing_cookie)) || []
-        cookie_flash += sanitize_flash(flash, unobtrusive_flash_keys)
+        cookie_flash = (existing_cookie && parse_cookie(existing_cookie))
+        cookie_flash = [] unless cookie_flash.is_a? Array
+        cookie_flash += self.sanitize_flash(flash, unobtrusive_flash_keys)
         cookie_flash.uniq.to_json
       end
 
       def parse_cookie(existing_cookie)
         JSON.parse(existing_cookie)
       rescue JSON::JSONError
-        nil
+        []
       end
     end
   end
